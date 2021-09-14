f734c931973dc933b59a6fa91406ee58d360d2b3
zeng715734199 <715734199@qq.com>
2021-09-14 22:03:32 +0800

    deploy

---
+++ precache-manifest.24cf04347bc1dda123af91ef53ab973f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c034319dd55b0648d33",
+    "url": "/Money-1-website/css/app.9019401a.css"
+  },
+  {
+    "revision": "ec4ae1e107f0669e2e7117b2e5dece9d",
+    "url": "/Money-1-website/index.html"
+  },
+  {
+    "revision": "4c034319dd55b0648d33",
+    "url": "/Money-1-website/js/app.b5fd5627.js"
+  },
+  {
+    "revision": "4149cc4cf4a79ff79d58",
+    "url": "/Money-1-website/js/chunk-vendors.83721ea5.js"
+  },
+  {
+    "revision": "02e9c9d455de36ce8eaf63626b307f5f",
+    "url": "/Money-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Money-1-website/robots.txt"
+  }
+]);
